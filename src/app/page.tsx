c6425e39fcfea49095f76c4d7c51d0eae4c2ac26
Pavel Moskov <pavel.moskov@gmail.com>
2024-11-11 00:35:48 +0300

    menuitem component and typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <Link
-      className="relative w-full max-w-[1140px] min-h-[400px] bg-[url('/background-pic.jpg')] bg-center bg-no-repeat bg-cover mx-auto overflow-hidden z-0 border border-white rounded-[0.5rem]"
+      className="relative w-full max-w-[1140px] min-h-[400px] bg-[url('/bikini/background-pic.jpg')] bg-center bg-no-repeat bg-cover mx-auto overflow-hidden z-0 border border-white rounded-[0.5rem]"
       href="/offers"            
     >
       <div className="bg-[rgba(255,226,240,0.4)] w-full h-full absolute left-0 top-0 flex flex-col justify-center items-center">
