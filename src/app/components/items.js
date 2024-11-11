ad4069df531e9c379cf9e28beb699c6df353e817
Pavel Moskov <pavel.moskov@gmail.com>
2024-11-11 14:40:43 +0300

    component card rendering

---
+++ src/app/components/items.js
@@ -0,0 +1,8 @@
+export const items = [
+    {"id":0,"price":4500, "name":"Купальник с топом красный", "url": "https://avatars.mds.yandex.net/get-mpic/11402506/2a0000018b6cef7dfbbf00b7da6c07801a57/optimize"},
+    {"id":1,"price":4300, "name":"Купальник раздельный бандо в горошек лавандовый", "url": "https://avatars.mds.yandex.net/get-mpic/1602935/img_id2305188682114279571.jpeg/optimize"},
+    {"id":2,"price":6000, "name":"Купальник раздельный с лифом бандо и высокими плавками хаки", "url": "https://avatars.mds.yandex.net/get-mpic/11047949/2a0000018c30779777b405308e5fdd08a4b9/optimize"},
+    {"id":3,"price":2800, "name":"Купальник раздельный с длинными завязками черный", "url": "https://avatars.mds.yandex.net/get-mpic/5234850/img_id8161579730788417289.jpeg/optimize"},
+    {"id":4, "price":5900, "name":"Туника аббая халат пляжная длинная с поясом", 'url': "https://avatars.mds.yandex.net/get-mpic/11722550/2a0000018b5cc9564c6dfeb774890e27d6ad/optimize"},
+    {"id":5, "price":4500,"name":"Купальник раздельный пуш-ап со стразами","url": "https://avatars.mds.yandex.net/get-mpic/11378054/2a0000018b6dfa113c0f5a3fba9c1b1522aa/optimize"},
+]
---
+++ src/app/offers/page.tsx
@@ -0,0 +1,14 @@
+import Card from "../components/card";
+
+export default function Offers() {
+  return (
+    <section className="relative w-full max-w-[1140px] min-h-[200px] bg-white mx-auto border border-[rgba(0,0,0,0.15)] rounded-[0.5rem] p-[20px] flex flex-wrap gap-[20px] justify-center items-center">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+     </section>
+  );
+};
